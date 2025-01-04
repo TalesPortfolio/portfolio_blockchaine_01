d2a5ed92efc6c08164b0c68a6d040d96a5e7c004
tales1982 <142450583+TalesPortfolio@users.noreply.github.com>
2025-01-04 15:04:36 +0100

    update

--- Grammy/contracts/script/DeployContract.s.sol
+++ Grammy/contracts/script/DeployContract.s.sol
@@ -1,18 +1,25 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
 
 import "forge-std/Script.sol";
-import "../src/Grammy.sol"; // Substitua pelo nome do seu contrato
+import "../src/Grammy.sol";
 
-contract DeployContract is Script {
+contract DeployGrammy is Script {
     function run() external {
-        vm.startBroadcast();
+        // Carregar chave privada do arquivo .env
+        string memory privateKey = vm.envString("PRIVATE_KEY");
 
-        // Deploy do contrato
+        // Iniciar a transmissão da transação
+        vm.startBroadcast(vm.envUint(privateKey));
+
+        // Fazer o deploy do contrato
         Grammy grammy = new Grammy();
 
-        console.log("Contract deployed at:", address(grammy));
+        // Exibir o endereço do contrato implantado
+        console.log("Grammy contract deployed at:", address(grammy));
 
+        // Finalizar a transmissão
         vm.stopBroadcast();
     }
 }
+
